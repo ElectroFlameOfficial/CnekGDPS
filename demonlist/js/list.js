310b1fe62afa5d366102801996065baaabf7f9fe
ElectroSonic <51268650+ElectroFlameOfficial@users.noreply.github.com>
2021-01-03 21:45:27 -0500

    Create list.js

---
+++ demonlist/js/list.js
@@ -0,0 +1,51 @@
+const d = {
+  "list": [
+    /*=================================================================================*/
+    {
+      "vids": [
+        {
+          "user": "",
+          "link": "",
+          "percent": "",
+          "hz": ""
+        },
+      ],
+      "name": "Psychotomimetic",
+      "author": "TFIBB",
+      "more": "none",
+      "id": "1916",
+      "pass": "",
+      "percentToQualify": 47,
+      "verificationVid": "https://youtu.be/xRPg4xr8A-c",
+      "key": 0
+    },
+    /*=================================================================================*/
+    {
+      "vids": [
+        {
+          "user": "KrisKing",
+          "link": "https://youtu.be/zthrRqFHjAc",
+          "percent": 100,
+          "hz": "120Hz"
+        },
+      ],
+      "name": "Thunderous Nostalgia",
+      "author": "TaehyunTXT [TFIBB]",
+      "more": "none",
+      "id": "1344",
+      "pass": "",
+      "percentToQualify": 54,
+      "verificationVid": "https://www.youtube.com/watch?v=gTEzOVh_sgM",
+      "key": 1
+    },
+    /*=================================================================================*/
+
+  ],
+  "version": [
+    2,
+    0,
+    0
+  ]
+};
+
+const list = d.list; const version = d.version;
